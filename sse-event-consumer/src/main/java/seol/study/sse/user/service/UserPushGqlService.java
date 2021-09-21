010dc8f1ebbcecbf1694165c56843ee871cf94cd
설연수 <seolnavy@naver.com>
2021-09-21 23:49:14 +0900

    GraphQL 연동

--- sse-event-consumer/src/main/java/seol/study/sse/user/service/UserPushGqlService.java
+++ sse-event-consumer/src/main/java/seol/study/sse/user/service/UserPushGqlService.java
@@ -3,29 +3,25 @@ package seol.study.sse.user.service;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
-import seol.study.sse.user.dto.UserPushResponseDto;
+import seol.study.sse.user.gql.userpush.UserPush;
 import seol.study.sse.user.repository.UserPushRepository;
 
 @Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
-public class UserPushService {
+public class UserPushGqlService {
 
 	private final UserPushRepository userPushRepository;
 
-	public Flux<UserPushResponseDto> connect(final String authToken) {
-		// TODO: Redis에 토큰 검증.
-
+	public Publisher<UserPush> userEvent(final String authToken) {
 		return userPushRepository.findByAuthTokenAndGreaterThanCreatedAt(authToken, LocalDateTime.now())
-				.map(UserPushResponseDto::from)
+				.map(UserPush::from)
 				.subscribeOn(Schedulers.boundedElastic())
 				.log();
 	}
-	
-
 }
