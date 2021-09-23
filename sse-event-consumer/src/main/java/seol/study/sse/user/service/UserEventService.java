a3f70c73149855ef1546350b26aae94697cf4847
설연수 <seolys@marketboro.com>
2021-09-23 23:49:39 +0900

    Repository or조건 적용

--- sse-event-consumer/src/main/java/seol/study/sse/user/service/UserEventService.java
+++ sse-event-consumer/src/main/java/seol/study/sse/user/service/UserEventService.java
@@ -21,11 +21,32 @@ public class UserEventService {
 	public Flux<UserEventResponseDto> connect(final String authToken) {
 		// TODO: Redis에 토큰 검증.
 
-		return userEventRepository.findByAuthTokenAndGreaterThanCreatedAt(authToken, LocalDateTime.now())
+		return userEventRepository.findByAuthTokenAndGreaterThanCreatedAt("test", authToken, LocalDateTime.now())
 				.map(UserEventResponseDto::from)
+				.takeUntil(dto -> dto.getEventType().isLogout())
 				.subscribeOn(Schedulers.boundedElastic())
 				.log();
 	}
 
+//	public Predicate<UserEventResponseDto> redisTokenExpiredDistinct() {
+//		AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+//		AtomicInteger atomicInteger = new AtomicInteger(0);
+//		return dto -> {
+//			System.out.println("atomicBoolean=" + atomicBoolean);
+//			System.out.println("atomicInteger=" + atomicInteger);
+//			if (atomicBoolean.get()) { // false일때 false리턴.
+//				return true;
+//			}
+//			if (dto.getEventType() != REDIS_TOKEN_EXPIRED) { // 토큰만료가 아닌경우 그대로 리턴.
+//				return false;
+//			}
+//			// 한번만 내보내고 그다음부터 false
+//			log.info("야호!!!!! 세션만료다!!");
+//			atomicInteger.incrementAndGet();
+//			return atomicBoolean.getAndSet(true);
+//		};
+//	}
 
 }
+
+
