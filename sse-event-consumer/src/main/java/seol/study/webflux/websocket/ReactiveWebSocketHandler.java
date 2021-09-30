e2309cc1d43fcf925384799d2957d62c3c8ef392
설연수 <seolys@marketboro.com>
2021-09-30 13:53:56 +0900

    웹소켓 테스트

--- sse-event-consumer/src/main/java/seol/study/webflux/websocket/ReactiveWebSocketHandler.java
+++ sse-event-consumer/src/main/java/seol/study/webflux/websocket/ReactiveWebSocketHandler.java
@@ -1,21 +1,25 @@
 package seol.study.webflux.websocket;
 
-import java.time.Duration;
 import java.util.function.Function;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.socket.CloseStatus;
 import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Slf4j
 @Component
 public class ReactiveWebSocketHandler implements WebSocketHandler {
 
+
 	@Override
 	public Mono<Void> handle(WebSocketSession session) {
+		log.info("===================================================");
+		log.info("session.isOpen()={}", session.isOpen());
+		log.info("session.getHandshakeInfo()={}", session.getHandshakeInfo());
+
 		// 1)
 //		return session.receive() // receive: 인바운드 메세지 스트림에 접근하고 커넥션을 닫으면 완료한다.
 //				.map(it -> {
@@ -33,15 +37,40 @@ public class ReactiveWebSocketHandler implements WebSocketHandler {
 //				.map(message -> session.textMessage("Echo: " + message));
 
 		// 3)
-		Flux<WebSocketMessage> output = session.receive()
-				.map(it -> {
-					log.info("[receive] message={}", it.getPayloadAsText());
-					return Flux.interval(Duration.ofSeconds(3L))
-							.map(s -> session.textMessage("Echo: " + s));
-				})
-				.flatMap(Function.identity());
+//		Flux<WebSocketMessage> output = session.receive()
+//				.map(it -> {
+//					log.info("[receive] message={}", it.getPayloadAsText());
+//					return Flux.interval(Duration.ofSeconds(3L))
+//							.map(s -> session.textMessage("Echo: " + s));
+//				})
+//				.flatMap(Function.identity());
 //		Flux<WebSocketMessage> output = Flux.interval(Duration.ofSeconds(3L))
 //				.map(s -> session.textMessage("Echo: " + s));
-		return session.send(output);
+
+		// 4)
+//		Flux<WebSocketMessage> output = session.receive() // receive: 인바운드 메세지 스트림에 접근하고 커넥션을 닫으면 완료한다.
+//				.map(it -> {
+//					String payloadAsText = it.getPayloadAsText();
+//					log.info("[receive] message={}", payloadAsText);
+//
+//					return session.textMessage(payloadAsText + "응답");
+//				});
+//		return session.send(output);
+
+		// 5)
+		Mono<Void> output = session.receive() // receive: 인바운드 메세지 스트림에 접근하고 커넥션을 닫으면 완료한다.
+				.map(it -> {
+					String payloadAsText = it.getPayloadAsText();
+					log.info("[receive] message={}", payloadAsText);
+
+					if ("quit".equals(payloadAsText) || "exit".equals(payloadAsText)) {
+						return session.close(CloseStatus.NORMAL);
+					}
+					WebSocketMessage webSocketMessage = session.textMessage(payloadAsText + "응답");
+					return session.send(Mono.just(webSocketMessage));
+				})
+				.flatMap(Function.identity())
+				.then();
+		return output;
 	}
 }
